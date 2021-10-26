a52b6a910348970d2bb0f8b8c09141ac372e455c
leog <leo_ga.work@outlook.com>
2021-10-26 15:41:47 -0500

    Re-doing "Venta" stuff with a dictionary

--- manage_models/static/venta.js
+++ manage_models/static/venta.js
@@ -1,19 +1,37 @@
+let ventas = 0;
+let data = {
+    cliente: '',
+    ventas: []
+};
+
+function evaluar_cliente(){
+    data[0] = $('#name_search').val();
+}
+
 $(function(){
     $('#name_search').autocomplete({
-        source : '/clientes/data/nombres/'
+        minLength: 0,
+        source : '/clientes/data/nombres/',
+        select : function(event, ui){
+            evaluar_cliente();
+        },
     });
     $('#product_search').autocomplete({
-        source : '/productos/data/nombres/'
+        minLength: 0,
+        source : '/productos/data/nombres/',
+        select : function(event, ui){
+            evaluar_cliente();
+        }
     });
     $('.ui-autocomplete').css({
         'list-style-type': 'none',
         'border': 'black solid thin',
-    })
-    $('.ui-helper-hidden-accessible').remove()
-
-    let ventas = 0;
+    });
+    $('.ui-helper-hidden-accessible').remove();
 
     $('#agregar_producto').click(function(){
+        evaluar_cliente();
+        ventas = ventas + 1;
         cliente = $('#name_search').val()
         producto = $('#product_search').val()
         producto_respuesta = $.ajax({
@@ -22,17 +40,13 @@ $(function(){
             type : 'get',
             datatype : 'json',
             success : function(response){
-                ventas++;
                 $('#tabla_productos').prepend(`
                 <tr>
                     <td>
-                        <p class="usuarios_tablecontent usuarios_espaciado">${cliente}</p>
+                        <p class="usuarios_tablecontent usuarios_espaciado" id="nombre_producto">${response.nombre}</p>
                     </td>
                     <td>
-                        <p class="usuarios_tablecontent usuarios_espaciado">${response.nombre}</p>
-                    </td>
-                    <td>
-                        <p class="usuarios_tablecontent usuarios_espaciado">${response.precio}</p>
+                        <p class="usuarios_tablecontent usuarios_espaciado id="precio_producto"">${response.precio}</p>
                     </td>
                     <td>
                         <input type="number" id="cantidad${ventas}">
@@ -41,24 +55,44 @@ $(function(){
                         <input type="number" id="descuento${ventas}">
                     </td>
                     <td>
-                        <button id="calcular_importe${ventas}" class="btn btn-success">Aceptar</button>
-                    </td>
-                    <td>
-                    <p class="usuarios_tablecontent usuarios_espaciado" id="importe${ventas}"></p>
+                    <p class="usuarios_tablecontent usuarios_espaciado importes" id="importe${ventas}"></p>
                     </td>
                 </tr>
                 `);
 
-                $(`#calcular_importe${ventas}`).click(function(){
-                    cantidad = $(`#cantidad${ventas}`).val()
+                $(`#cantidad${ventas}`).change(function(){
+                    evaluar_cliente();
+                    cantidad = $(this).val()
                     descuento = $(`#descuento${ventas}`).val()
                     importe = parseFloat((cantidad * response.precio) - ( ( (response.precio * cantidad) / 100 ) * descuento ));
-                    $(`#importe${ventas}`).append(importe);
-                    $(`#calcular_importe${ventas}`).remove();
-                    $(`#descuento${ventas}`).prop('disabled', true);
-                    $(`#cantidad${ventas}`).prop('disabled', true);
+                    $(`#importe${ventas}`).html('')
+                    $(`#importe${ventas}`).append(Math.round((importe + Number.EPSILON) * 100) / 100);
+                    total = 0;
+                    $('.importes').each(function(){
+                        total = total + parseFloat($(this).text());
+                    });
+                    $('#total').text(Math.round((total + Number.EPSILON) * 100) / 100);
+                    total = 0;
+                    importe = 0;
                 });
-            },
+                $(`#descuento${ventas}`).change(function(){
+                    evaluar_cliente();
+                    cantidad = $(`#cantidad${ventas}`).val()
+                    descuento = $(this).val()
+                    importe = parseFloat((cantidad * response.precio) - ( ( (response.precio * cantidad) / 100 ) * descuento ));
+                    $(`#importe${ventas}`).html('')
+                    $(`#importe${ventas}`).append(Math.round((importe + Number.EPSILON) * 100) / 100);
+                    total = 0;
+                    $('.importes').each(function(){
+                        total = total + parseFloat($(this).text());
+                    });
+                    $('#total').text(Math.round((total + Number.EPSILON) * 100) / 100);
+                    total = 0;
+                    importe = 0;
+                });
+            }
         });
     });
 });
+
+// CORREGIR ERROR. CUANDO SE INTENTA EDITAR LOS DATOS DE UNA VENTA PASADA, SE ALTERA LA MÁS RECIENTE.
